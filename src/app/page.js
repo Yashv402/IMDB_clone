6eecceb21197e76bce4279b273f6c635fe490001
Yashv402 <112461733+Yashv402@users.noreply.github.com>
2024-05-29 15:35:41 +0530

    trending, top rated page and movie page added

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ const API_KEY = process.env.API_KEY;
 
 import Results from "@/components/Results";
 
-export default async function Home(searchParams) {
+export default async function Home({searchParams}) {
+  console.log(searchParams.genre);
   const genre = searchParams.genre || "fetchTrending";
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page=1`
+    // {next: { revalidate:10000 }}
   );
   const data = await res.json();
   if (!res.ok) {
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
+import Card from './Card'
 
 function Results({results}) {
-    console.log(results)
   return (
-    <div>{
+    <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto p-6'>{
         results.map((result)=>(
-            <div key={result.id}>
-                <h2>{result.original_title || result.original_name}</h2>
-            </div>
+              <Card key ={result.id} result = {result}/>
         ))}
     </div>
   )
